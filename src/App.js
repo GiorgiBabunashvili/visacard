c3506e85bfcbbcaaea661f84b40c91754863248e
shanME <giorgie103188@gmail.com>
2024-09-23 17:30:03 +0400

    userForm

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import UserForm from "./components/Form";
 import CardBackView from "./components/VisaCard/back";
 import CardFrontView from "./components/VisaCard/Front";
 
@@ -5,7 +6,8 @@ function App() {
   return (
     <div className="h-screen flex flex-col justify-center items-center">
       <CardFrontView />
-      <CardBackView />
+      {/* <CardBackView /> */}
+      <UserForm />
     </div>
   );
 }
